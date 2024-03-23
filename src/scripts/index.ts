67209e2d6b60bf40ab3a7a861352fa8560569a6e
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2024-03-23 23:30:06 +0100

    created math lib

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,8 @@
 import { Vector2 } from './types/Vector2.js';
+import { toDegrees, toRadians } from './utils/Math.js';
 
 console.log('init');
 let x = new Vector2();
 console.log(x);
+console.log(toRadians(60));
+console.log(toDegrees(1));
---
+++ src/scripts/utils/Math.ts
@@ -0,0 +1,2 @@
+export const toDegrees = (angle: number) => angle * (180 / Math.PI);
+export const toRadians = (angle: number) => angle * (Math.PI / 180);
