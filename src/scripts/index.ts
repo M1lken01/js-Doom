beb407d77253c2a414325cf6b41283e8d8420e82
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2024-03-24 17:38:19 +0100

    new model storage

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,4 @@
-import { Model } from './types/Model.js';
+import { Cube } from './models/cube.js';
 import { Player } from './types/Player.js';
 import { Vector3 } from './types/Vector3.js';
 import { render } from './utils/Render.js';
@@ -11,35 +11,9 @@ let player: Player;
 
 console.log('init');
 
-const cubeSize = 100;
-const cube = new Model(
-  [
-    new Vector3(0, 0, 0),
-    new Vector3(0, cubeSize, 0),
-    new Vector3(0, cubeSize, cubeSize),
-    new Vector3(0, 0, cubeSize),
-    new Vector3(cubeSize, 0, 0),
-    new Vector3(cubeSize, cubeSize, 0),
-    new Vector3(cubeSize, cubeSize, cubeSize),
-    new Vector3(cubeSize, 0, cubeSize),
-  ],
-  [
-    [4, 5, 6], // Back face
-    [4, 6, 7], // Back face (continued)
-    [0, 4, 7], // Left face
-    [0, 7, 3], // Left face (continued)
-    [1, 5, 6], // Right face
-    [1, 6, 2], // Right face (continued)
-    [0, 1, 5], // Top face
-    [0, 5, 4], // Top face (continued)
-    [2, 6, 7], // Bottom face
-    [2, 7, 3], // Bottom face (continued)
-    [0, 1, 2], // Front face
-    [0, 2, 3], // Front face (continued)
-  ],
-  new Vector3(),
-  new Vector3(cubeSize / 2, cubeSize / 2, cubeSize / 2),
-);
+const cube = new Cube(100).model;
+const cube2 = new Cube(100, new Vector3(0, 0, 200)).model;
+const cube3 = new Cube(100, new Vector3(0, -100, 100)).model;
 
 function update() {
   const x = parseFloat((document.getElementById('x') as HTMLInputElement).value.toString());
@@ -47,7 +21,7 @@ function update() {
   const z = parseFloat((document.getElementById('z') as HTMLInputElement).value.toString());
   cube.position = new Vector3(x, y, z);
 
-  render(player, [cube], fov);
+  render(player, [cube, cube2, cube3], fov);
 }
 
 function init() {
---
+++ src/scripts/models/cube.ts
@@ -0,0 +1,41 @@
+import { Model } from '../types/Model';
+import { Vector3 } from '../types/Vector3';
+
+export class Cube {
+  size: number;
+  position: Vector3;
+  model: Model;
+
+  constructor(size: number = 100, position: Vector3 = new Vector3()) {
+    this.size = size;
+    this.position = position;
+    this.model = new Model(
+      [
+        new Vector3(0, 0, 0),
+        new Vector3(0, size, 0),
+        new Vector3(0, size, size),
+        new Vector3(0, 0, size),
+        new Vector3(size, 0, 0),
+        new Vector3(size, size, 0),
+        new Vector3(size, size, size),
+        new Vector3(size, 0, size),
+      ],
+      [
+        [4, 5, 6], // Back face
+        [4, 6, 7], // Back face (continued)
+        [0, 4, 7], // Left face
+        [0, 7, 3], // Left face (continued)
+        [1, 5, 6], // Right face
+        [1, 6, 2], // Right face (continued)
+        [0, 1, 5], // Top face
+        [0, 5, 4], // Top face (continued)
+        [2, 6, 7], // Bottom face
+        [2, 7, 3], // Bottom face (continued)
+        [0, 1, 2], // Front face
+        [0, 2, 3], // Front face (continued)
+      ],
+      new Vector3(0, 0, 200),
+      new Vector3(size / 2, size / 2, size / 2),
+    );
+  }
+}
